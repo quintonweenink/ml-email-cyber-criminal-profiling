b18a63b04a4cf22d3d9393fdcd6fbf671bd8bc55
Quinton Weenink <qweenink@gmail.com>
2018-04-15 21:51:14 +0200

    Removed string parser

--- experiments/emailHeader.py
+++ experiments/emailHeader.py
@@ -4,17 +4,9 @@ from email.policy import default
 
 messagefile = "../maildir/maildir/allen-p/inbox/1."
 
-# If the e-mail headers are in a file, uncomment these two lines:
+# Read in email file:
 with open(messagefile, 'rb') as fp:
     headers = BytesParser(policy=default).parse(fp)
-
-#  Or for parsing headers in a string (this is an uncommon operation), use:
-# headers = Parser(policy=default).parsestr(
-#         'From: Foo Bar <user@example.com>\n'
-#         'To: <someone_else@example.com>\n'
-#         'Subject: Test message\n'
-#         '\n'
-#         'Body would go here\n')
 
 #  Now the header items can be accessed as a dictionary:
 print('To: {}'.format(headers['to']))
@@ -24,3 +16,6 @@ print('Subject: {}'.format(headers['subject']))
 # You can also access the parts of the addresses:
 print('Recipient username: {}'.format(headers['to'].addresses[0].username))
 print('Sender name: {}'.format(headers['from'].addresses[0].display_name))
+
+# The email:
+#print(headers)
